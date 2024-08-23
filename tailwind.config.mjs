b6deffcf7f0de5dc8fe806f8d047becbd9b5f356
Kaique Savi <kaiquesavi1@gmail.com>
2024-08-23 13:19:32 -0300

    Fix: ajustes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,7 +10,17 @@ export default {
 			fontFamily: {
 				'Bahnschrift': 'Bahnschrift',
 				'Montserrat': 'Montserrat'
-			}
+			},
+			animation: {
+				fade: 'fadeIn .5s ease-in-out',
+				fadeOut: 'fadeOut .5s ease-in-out',
+			  },
+			  keyframes: {
+				fadeIn: {
+					from: { opacity: 0 },
+					to: { opacity: 1 },
+				},
+			},
 		},
 	},
 	plugins: [],
